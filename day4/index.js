823b3949457b104d1171bf7bcb246cffebc87f88
Firecreper2 <64336537+Firecreper2@users.noreply.github.com>
2022-12-04 00:51:10 -0500

    day4 2star

--- day4/index.js
+++ day4/index.js
@@ -17,8 +17,6 @@ pairs.forEach((array) => {
 	let range1 = [parseInt(pairsArray[0].split("-")[0]), parseInt(pairsArray[0].split("-")[1])]
 	let range2 = [parseInt(pairsArray[1].split("-")[0]), parseInt(pairsArray[1].split("-")[1])]
 
-	console.log(range1,range2,(range1[1] >= range2[0] && range1[0] <= range2[0]) || (range1[0] <= range2[1] && range1[1] >= range2[1]))
-
 	if((range1[1] >= range2[0] && range1[0] <= range2[1]))
 		amountOfPairs++
 })
